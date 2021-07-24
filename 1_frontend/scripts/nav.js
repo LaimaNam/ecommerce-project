e57efa6d90ec58db23c0a64cce9228f54c086da2
LaimaNam <laima.namajunaite@gmail.com>
2021-07-25 01:39:28 +0300

    styling langing page

--- 1_frontend/scripts/nav.js
+++ 1_frontend/scripts/nav.js
@@ -1,6 +1,22 @@
 //variables
+const headerTop = document.querySelector('header');
 
+const navigationShopLinksToggler = document.querySelector(
+  '.navigation-shop-link'
+);
+const shopLinks = document.querySelector('.shop-hover-links-wrapper');
 //functions
+// const renderTopNavigation = () => {};
 
 //events
-document.addEventListener('DOMContentLoaded', renderNavigation);
+navigationShopLinksToggler.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (shopLinks.style.display === 'none') {
+    shopLinks.style.display = 'block';
+    shopLinks.style.top = '25px';
+  } else {
+    shopLinks.style.display = 'none';
+  }
+});
+// document.addEventListener('DOMContentLoaded', renderTopNavigation);
